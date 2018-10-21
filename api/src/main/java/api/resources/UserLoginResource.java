53d2bb6b7e663c5ace8d98d5998f5d10a36af115
Blazka Blatnik <bb3172@student.uni-lj.si>
2018-10-21 21:23:28 +0200

    #1: add login basic functionality

--- api/src/main/java/api/resources/UserLoginResource.java
+++ api/src/main/java/api/resources/UserLoginResource.java
@@ -1,6 +1,5 @@
 package api.resources;
 
-
 import io.swagger.v3.oas.annotations.tags.Tag;
 import io.swagger.v3.oas.annotations.tags.Tags;
 
@@ -11,18 +10,20 @@ import javax.ws.rs.core.Response;
 
 @Consumes(MediaType.APPLICATION_JSON)
 @Produces(MediaType.APPLICATION_JSON)
-@Path("register")
+@Path("login")
 @ApplicationScoped
 @Tags(value = @Tag(name = "authentication"))
-public class UserRegisterResource{
+public class UserLoginResource {
 
-    @PUT
+    @POST
     public Response registerUser(
             @QueryParam("username") String username,
-            @QueryParam("email") String email,
             @QueryParam("password") String password){
 
-        return Response.ok().build();
+        if (username.equals("user") && password.equals("123"))
+            return Response.ok().build();
+        else
+            return Response.status(Response.Status.UNAUTHORIZED).build();
 
     }
 }
