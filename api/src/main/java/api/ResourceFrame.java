836368dfb4505618334ac76454c354d067e63b42
Blazka Blatnik <bb3172@student.uni-lj.si>
2018-11-04 22:53:30 +0100

    register service to consul

--- api/src/main/java/api/ResourceFrame.java
+++ api/src/main/java/api/ResourceFrame.java
@@ -1,6 +1,7 @@
 package api;
 
 import com.kumuluz.ee.cors.annotations.CrossOrigin;
+import com.kumuluz.ee.discovery.annotations.RegisterService;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Contact;
 import io.swagger.v3.oas.annotations.info.Info;
@@ -11,4 +12,5 @@ import javax.ws.rs.ApplicationPath;
 @OpenAPIDefinition(info = @Info(title = "SkipRope® API", version = "v1", contact = @Contact, license = @License))
 @ApplicationPath("api")
 @CrossOrigin(supportedMethods = "GET, POST, PUT, DELETE, HEAD, OPTIONS", exposedHeaders = "X-Total-Count, Content-Disposition, X-Export-Filename")
+@RegisterService(value = "user-service", ttl = 20, pingInterval = 15, environment = "dev", version = "1.0.0", singleton = false)
 public class ResourceFrame extends javax.ws.rs.core.Application { }
